4631477ae3ffbb541fd5ee881d1e7f7f297ebfdb
Ayush Jain <77970015+not4YU5H@users.noreply.github.com>
2022-10-06 21:33:04 +0530

    Added user authentication functionality

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ const {
 } = require("./config/config");
 
 const postRouter = require("./routes/postRoutes");
+const userRouter = require("./routes/userRoutes")
 
 const app = express();
 
@@ -34,6 +35,7 @@ app.get("/", (req, res) => {
 
 //localhost:3000/api/v1/post/
 app.use("/api/v1/posts", postRouter);
+app.use("/api/v1/users", userRouter);
 
 const port = process.env.PORT || 3000;
 
